69eece78864b4a40e41719b168fb0c297b94f884
olafgeppert-droid <olaf.geppert@googlemail.com>
2025-09-18 13:29:10 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,55 +1,55 @@
-/** @jsxImportSource react */
-import React, { useState } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 
 // Komponenten importieren
-import Header from './components/Header.jsx';
-import Navigation from './components/Navigation.jsx';
-import Toolbar from './components/Toolbar.jsx';
-import StudentDetails from './components/MainContent.jsx';
-import DayDetails from './components/MainContent.jsx';
-import StudentModal from './components/StudentModal.jsx';
-import EntryModal from './components/EntryModal.jsx';
-import SettingsModal from './components/SettingsModal.jsx';
-import StatisticsModal from './components/StatisticsModal.jsx';
-import HelpModal from './components/HelpModal.jsx';
+import Header from './components/Header.jsx'
+import Navigation from './components/Navigation.jsx'
+import Toolbar from './components/Toolbar.jsx'
+import StudentDetails from './components/StudentDetails.jsx'
+import DayDetails from './components/DayDetails.jsx'
+import StudentModal from './components/StudentModal.jsx'
+import EntryModal from './components/EntryModal.jsx'
+import SettingsModal from './components/SettingsModal.jsx'
+import HelpModal from './components/HelpModal.jsx'
+
+import './styles/main.css'
+import './styles/components.css'
 
 const App = () => {
-    const [selectedStudent, setSelectedStudent] = useState(null);
-    const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedStudent, setSelectedStudent] = React.useState(null)
+  const [selectedDate, setSelectedDate] = React.useState(new Date())
 
-    return (
-        <div className="app">
-            <Header />
-            <Navigation />
-            <Toolbar
-                selectedStudent={selectedStudent}
-                selectedDate={selectedDate}
-                onAddStudent={() => {}}
-                onEditStudent={() => {}}
-                onAddEntry={() => {}}
-                onEditEntry={() => {}}
-                onPrint={() => {}}
-                onExport={() => {}}
-                onImport={() => {}}
-                onUndo={() => {}}
-                onRedo={() => {}}
-                canUndo={false}
-                canRedo={false}
-            />
-            <main className="main">
-                <StudentDetails student={selectedStudent} />
-                <DayDetails date={selectedDate} />
-            </main>
-            <StudentModal />
-            <EntryModal />
-            <SettingsModal />
-            <StatisticsModal />
-            <HelpModal />
-        </div>
-    );
-};
+  return (
+    <div className="app">
+      <Header />
+      <Navigation />
+      <Toolbar
+        selectedStudent={selectedStudent}
+        selectedDate={selectedDate}
+        onAddStudent={() => {}}
+        onEditStudent={() => {}}
+        onAddEntry={() => {}}
+        onEditEntry={() => {}}
+        onPrint={() => {}}
+        onExport={() => {}}
+        onImport={() => {}}
+        onUndo={() => {}}
+        onRedo={() => {}}
+        canUndo={false}
+        canRedo={false}
+      />
+      <main className="main">
+        <StudentDetails student={selectedStudent} />
+        <DayDetails date={selectedDate} />
+      </main>
+      <StudentModal />
+      <EntryModal />
+      <SettingsModal />
+      <HelpModal />
+    </div>
+  )
+}
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+const container = document.getElementById('root')
+const root = createRoot(container)
+root.render(<App />)
