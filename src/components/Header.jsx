a6a3d23b3529c512262eb6a13101968ac43281a6
olafgeppert-droid <olaf.geppert@googlemail.com>
2025-09-18 19:47:16 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Header.css';
 
 const Header = () => {
   const currentDate = new Date().toLocaleDateString('de-DE', {
@@ -11,14 +10,14 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-left">
-        {/* Hier steht Ihr bestehender Header-Inhalt, z.B.: */}
+        {/* Hier steht Ihr bestehender Header-Inhalt */}
         <h1>Ihre Anwendung</h1>
         {/* Navigation oder andere Elemente */}
       </div>
       
       <div className="header-right">
-        <div className="date">{currentDate}</div>
-        <div className="version">Version 1.4.0</div>
+        <div className="header-date">{currentDate}</div>
+        <div className="header-version">Version 1.4.0</div>
       </div>
     </header>
   );
