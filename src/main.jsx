b7925e5c32b067aee5b94df4b32f88c81bf82b23
olafgeppert-droid <olaf.geppert@googlemail.com>
2025-09-18 17:57:35 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './app.jsx'
 import './styles/main.css'
 import './styles/components.css'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
-root.render(<App />)
+root.render(<app />)
