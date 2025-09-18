a76ed16e5e6244444ad6b1e929882ad2e23ad511
olafgeppert-droid <olaf.geppert@googlemail.com>
2025-09-18 19:45:19 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
+import './Header.css';
 
-const Header = ({ onMenuClick }) => {
-    const currentDate = new Date().toLocaleDateString('de-DE', {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-    });
+const Header = () => {
+  const currentDate = new Date().toLocaleDateString('de-DE', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  });
 
-    const appVersion = "1.3.0"; // Versionsnummer
-
-    return (
-        <header className="header">
-            <button className="hamburger-menu" onClick={onMenuClick}>☰</button>
-            <h1>Dokumentation pädagogische Arbeit - Irina Geppert</h1>
-            <div className="header-info">
-                <span className="header-date">{currentDate}</span>
-                <span className="header-version">{appVersion}</span>
-            </div>
-        </header>
-    );
+  return (
+    <header className="header">
+      <div className="header-left">
+        {/* Hier steht Ihr bestehender Header-Inhalt, z.B.: */}
+        <h1>Ihre Anwendung</h1>
+        {/* Navigation oder andere Elemente */}
+      </div>
+      
+      <div className="header-right">
+        <div className="date">{currentDate}</div>
+        <div className="version">Version 1.4.0</div>
+      </div>
+    </header>
+  );
 };
 
-// Wichtiger Unterschied: ES-Modul Export
 export default Header;
