eecf6ef82391d837c18da8a38ad85105c90298d5
olafgeppert-droid <olaf.geppert@googlemail.com>
2025-09-20 03:09:52 +0200

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Header = ({ onMenuClick }) => {
+  const currentDate = new Date().toLocaleDateString('de-DE', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  });
+
+  return (
+    <header className="header">
+      <button className="hamburger-menu" onClick={onMenuClick}>
+        ☰
+      </button>
+      
+      <div className="header-center">
+        <h1 className="header-title">Dokumentation pädagogische Arbeit - Irina Geppert</h1>
+      </div>
+      
+      <div className="header-right">
+        <div className="header-date">{currentDate}</div>
+        <div className="header-version">Version 1.6.0</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
