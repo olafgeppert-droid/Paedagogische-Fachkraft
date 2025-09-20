7ba96339474fb1a07a857be543678ee591508b53
olafgeppert-droid <olaf.geppert@googlemail.com>
2025-09-20 20:54:28 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pädagogische-dokumentation/',
+  base: '/Paedagogische-Fachkraft/',
   build: {
     outDir: 'dist',
     sourcemap: false
