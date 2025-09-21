f67a3659009a15a3c832253539d0bba472f3fe7a
olafgeppert-droid <olaf.geppert@googlemail.com>
2025-09-21 22:29:45 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = ({ onMenuClick }) => {
       
       <div className="header-right">
         <div className="header-date">{currentDate}</div>
-        <div className="header-version">Version 1.7.7</div>
+        <div className="header-version">Version 1.7.8</div>
       </div>
     </header>
   );
