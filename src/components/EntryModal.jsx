d814f6dd39298970ad80b0aac961297362dcd1a2
olafgeppert-droid <olaf.geppert@googlemail.com>
2025-09-29 13:32:10 +0200

    Add files via upload

--- src/components/EntryModal.jsx
+++ src/components/EntryModal.jsx
@@ -133,7 +133,8 @@ const EntryModal = ({ onClose, onSave, masterData, student, date, existingEntry 
                         {/* Erfolg (Textfeld) */}
                         <div className="form-group">
                             <label className="form-label">Erfolg</label>
-                            <textarea                                
+                            <input
+                                type="text"
                                 className="form-input"
                                 value={formData.erfolg}
                                 onChange={(e) => handleChange('erfolg', e.target.value)}
